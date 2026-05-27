95c5b5a8c0543e2ba7261e400000795c437e8057
Lakshay7118 <lakshyamehra321@gmail.com>
2026-05-27 11:40:19 +0530

    template msg fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['192.168.0.132', '192.168.29.140'],  // ← add this
+  allowedDevOrigins: ['192.168.0.132', '192.168.29.140', '10.132.51.60'],  // ← add this
   // ...your other config
 };
 
