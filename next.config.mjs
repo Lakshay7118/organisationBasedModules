5beb94f75c488376d6207feda7132a8e0a194256
Lakshay7118 <lakshyamehra321@gmail.com>
2026-06-29 17:29:01 +0530

    chnge

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['172.16.1.98', '192.168.29.140', '10.132.51.60'],  // ← add this
+  allowedDevOrigins: ['172.16.1.98', '192.168.29.140', '10.132.51.60' ],  // ← add this
   // ...your other config
 };
 
